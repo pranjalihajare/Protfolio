d283658d8459f3324e860b4257023c6e415b7ff6
Pranjalihajare <134865897+pranjalihajare@users.noreply.github.com>
2025-06-21 21:45:27 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -50,7 +50,7 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="www.linkedin.com/in/pranjali-hajare-aa1a10277"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/pranjali-hajare-aa1a10277/"><img src={navIcon1} alt="" /></a>
                 <a href="https://github.com/pranjalihajare"><img src={navIcon2} alt="" /></a>
                 {/* <a href="#"><img src={navIcon3} alt="" /></a> */}
               </div>
